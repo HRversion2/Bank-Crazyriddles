dd28f1b0bf447de3eb827504e3644833e283ba97
Roy Park <roypark@roys-mbp.lan>
2022-11-20 14:28:44 -0800

    init

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Congratulations! You have successfully given us money, and we will use it for good use (A lamborgini)");
+
+let total = 1000;
+
+document.querySelector("#bank-form").addEventListener("click", function(e) {
+    e.preventDefault();
+
+    // get the inputs 
+    const deposit = document.querySelector("#deposit").value;
+
+
+    // do the math
+
+
+    // show the result
+});
